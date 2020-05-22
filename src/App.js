436ae07fc50e3a71d2e84ff66cf02e63556bfe4e
Axel Obscura <axosar@gmail.com>
2020-05-21 19:24:47 -0500

    Estructura

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <Row>
+          <Col sm={2} xs={2} className="p-0 m-0" style={{ background: '#f2f2f2' }}>
+            prueba
+          </Col>
+          <Col sm={10} xs={10} className="p-0 pb-5 m-0">
+            prueba
+            {/*
+            <Switch>
+              <Route exact path="/" component={Intro} />
+            </Switch>
+             */}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
