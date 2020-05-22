f1ac3ec986eb50320fd90f28e826d08afcbda4e4
Axel Obscura <axosar@gmail.com>
2020-05-21 19:31:24 -0500

    build 2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/subastasdelsur/precache-manifest.f3c01ed2158b4ff55aab5247bbb11920.js"
+  "/expedientemedico/precache-manifest.a8f9ec70b91cac8e5215f2a9865fa448.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/subastasdelsur/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/expedientemedico/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
